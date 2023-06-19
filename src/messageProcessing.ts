aa5d9a47776394b70fd14fdae019892856857c97
haphut <haphut@mistmap.com>
2023-06-19 16:25:20 +0300

    Simplify Pulsar message acknowledgment

--- src/messageProcessing.ts
+++ src/messageProcessing.ts
@@ -2,7 +2,6 @@ import type mqtt from "async-mqtt";
 import type pino from "pino";
 import type Pulsar from "pulsar-client";
 import type { MqttConfig } from "./config";
-import transformUnknownToError from "./util";
 
 const keepProcessingMessages = async (
   logger: pino.Logger,
@@ -18,42 +17,25 @@ const keepProcessingMessages = async (
     nRecentMessages = 0;
   }, 1_000 * logIntervalInSeconds);
 
-  const onPublishFulfilled = (
+  const processPulsarMessage = async (
     consumer: Pulsar.Consumer,
     message: Pulsar.Message
   ) => {
-    return () => {
-      nRecentMessages += 1;
-      // In case of an error, exit via the listener on unhandledRejection.
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      consumer.acknowledge(message);
-    };
-  };
-
-  const onPublishRejected = () => {
-    // any is the type for onRejected of then().
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (reason: any) => {
-      const err = transformUnknownToError(reason);
-      logger.fatal({ err }, "Publishing to MQTT failed");
-      // Aim to exit.
-      throw reason;
-    };
+    const data = message.getData();
+    const properties = message.getProperties();
+    const fullTopic = topic + (properties["topicSuffix"] ?? "");
+    await mqttClient.publish(fullTopic, data, publishOptions);
+    nRecentMessages += 1;
+    await consumer.acknowledge(message);
   };
 
   // Errors are handled on the main level.
   /* eslint-disable no-await-in-loop */
   for (;;) {
     const pulsarMessage = await pulsarConsumer.receive();
-    const data = pulsarMessage.getData();
-    const properties = pulsarMessage.getProperties();
-    const fullTopic = topic + (properties["topicSuffix"] ?? "");
-    mqttClient
-      .publish(fullTopic, data, publishOptions)
-      .then(
-        onPublishFulfilled(pulsarConsumer, pulsarMessage),
-        onPublishRejected()
-      );
+    // In case of an error, exit via the listener on unhandledRejection.
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    processPulsarMessage(pulsarConsumer, pulsarMessage);
   }
   /* eslint-enable no-await-in-loop */
 };
