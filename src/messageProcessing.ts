96f8104a2eb7a7446bf33766ae09a7c00c165fa6
haphut <haphut@mistmap.com>
2023-08-21 15:27:23 +0300

    fix: Use async/await again
    The refactoring in #41 gained nothing. Here we return to the previous
    code structure and add a comment to describe why it is expected to work.

--- src/messageProcessing.ts
+++ src/messageProcessing.ts
@@ -17,27 +17,32 @@ const keepProcessingMessages = async (
     nRecentMessages = 0;
   }, 1_000 * logIntervalInSeconds);
 
-  const processPulsarMessage = (
+  const processPulsarMessage = async (
     consumer: Pulsar.Consumer,
     message: Pulsar.Message
   ) => {
     const data = message.getData();
     const properties = message.getProperties();
     const fullTopic = topic + (properties["topicSuffix"] ?? "");
-    // In case of an error, exit via the listener on unhandledRejection.
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    mqttClient.publish(fullTopic, data, publishOptions).then(() => {
-      nRecentMessages += 1;
-      // In case of an error, exit via the listener on unhandledRejection.
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      consumer.acknowledge(message).then(() => {});
-    });
+    await mqttClient.publish(fullTopic, data, publishOptions);
+    nRecentMessages += 1;
+    await consumer.acknowledge(message);
   };
 
   // Errors are handled on the main level.
   /* eslint-disable no-await-in-loop */
   for (;;) {
     const pulsarMessage = await pulsarConsumer.receive();
+    // To utilize concurrency and to not limit throughput unnecessarily, we
+    // should _not_ await processPulsarMessage. Instead, Promises are handled in
+    // order by Node.js. The Promise will rely on the internal message queue in
+    // mqtt.js and MQTT-over-TCP to ensure that first copies of each MQTT
+    // message with QoS 1 stay in order and that Pulsar messages are
+    // acknowledged in order. Therefore here we can receive the next Pulsar
+    // message right away.
+    //
+    // In case of an error, exit via the listener on unhandledRejection.
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     processPulsarMessage(pulsarConsumer, pulsarMessage);
   }
   /* eslint-enable no-await-in-loop */
