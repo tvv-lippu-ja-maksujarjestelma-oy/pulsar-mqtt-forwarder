b9b0d2428a832277803362eea71bf567fa83012d
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2023-09-04 10:09:57 +0300

    feat: Add structured logging for GCP

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import type mqtt from "async-mqtt";
-import pino from "pino";
+import type pino from "pino";
 import type Pulsar from "pulsar-client";
 import { getConfig } from "./config";
+import { createLogger } from "./gcpLogging";
 import createHealthCheckServer from "./healthCheck";
 import { createPulsarClient, createPulsarConsumer } from "./pulsar";
 import transformUnknownToError from "./util";
@@ -87,17 +88,7 @@ const exitGracefully = async (
   /* eslint-enable @typescript-eslint/no-floating-promises */
   const serviceName = "pulsar-mqtt-forwarder";
   try {
-    const logger = pino(
-      {
-        name: serviceName,
-        timestamp: pino.stdTimeFunctions.isoTime,
-        redact: { paths: ["pid"], remove: true },
-        // As logger is started before config is created, read the level from
-        // env.
-        level: process.env["PINO_LOG_LEVEL"] ?? "info",
-      },
-      pino.destination({ sync: true })
-    );
+    const logger = createLogger({ name: serviceName });
 
     let setHealthOk: (isOk: boolean) => void;
     let closeHealthCheckServer: () => Promise<void>;
